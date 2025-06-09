6b54384f657be472d5681ed1f557524befdf58da
Adja Coura Ndour <adja-coura.ndour@ism.edu.sn>
2025-06-09 17:23:04 +0000

    angular

--- src/app/pages/admin/sessions/sessionsDetails/sessionDetails.component.ts
+++ src/app/pages/admin/sessions/sessionsDetails/sessionDetails.component.ts
@@ -1,39 +1,3 @@
-// import { Component, inject, OnInit } from '@angular/core';
-// import { Observable } from 'rxjs';
-// import { ActivatedRoute } from '@angular/router';
-// import { CommonModule } from '@angular/common';
-// import { FormsModule } from '@angular/forms';
-// import { SessionService } from '../../../../shared/services/impl/session.service';
-// import { PointageModel } from '../../../../shared/models/pointage.model';
-
-// @Component({
-//   selector: 'app-session-details',
-//   imports: [CommonModule, FormsModule],
-//   templateUrl: './sessions.component.html',
-//   styleUrl: './sessions.component.css'
-// })
-// export class SessionsComponent implements OnInit {
-
-//   private sessionService:SessionService = inject(SessionService);
-
-//   pointages$: Observable<any> = new Observable();
-//   detailspointages?: PointageModel;
-
-//   constructor(private route: ActivatedRoute) { }
-
-//   ngOnInit(): void {
-//     const sessionId = this.route.snapshot.paramMap.get('id');
-//     this.pointages$ = this.sessionService.getListeAbsences(Number(sessionId));
-
-//     this.pointages$.subscribe(data => {
-//       console.log("absence récupéré :", data);
-//       this.detailspointages = data;
-//       console.log("Détails de l'pointages :", this.detailspointages);
-    
-//     });
-//   }
-// }
-
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -45,29 +9,6 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 
-// @Component({
-//   selector: 'app-session-details',
-//   templateUrl: './sessionDetails.component.html',
-//   imports: [SessionComponent],
-// })
-// export class SessionDetailsComponent implements OnInit {
-
-//   private sessionService = inject(SessionService);
-//   private pointageService = inject(PointageService);
-//   private route = inject(ActivatedRoute);
-
-//   pointages: PointageModel[] = [];
-
-//   ngOnInit(): void {
-//     const sessionId = Number(this.route.snapshot.paramMap.get('id'));
-    
-//     this.pointageService.getAllPointagesDuneSessionDuJour(sessionId)
-//       .subscribe((data: PointageModel[]) => {
-//         this.pointages = data;
-//         console.log("Pointages récupérés :", this.pointages);
-//       });
-//   }
-// }
 @Component({
   selector: 'app-session-details',
   templateUrl: './sessionDetails.component.html',
