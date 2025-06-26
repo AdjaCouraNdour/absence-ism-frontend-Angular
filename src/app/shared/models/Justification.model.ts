866b16bf435520057e45c19035a96c2b873a0db5
AdjaCouraNdour <167907403+AdjaCouraNdour@users.noreply.github.com>
2025-06-26 14:53:48 +0000

    Update Justification.model.ts

--- src/app/shared/models/Justification.model.ts
+++ src/app/shared/models/Justification.model.ts
@@ -4,7 +4,7 @@ export interface JustificationModel {
       classeEtudiant : string;
       absenceId : string;
       commentaire : string;
-      fichierUrl : 'https://res.cloudinary.com/dn1bzgrqk/image/upload/v1749754284/qhsrv3tunp6oxoj8ihma.png';
+      fichierUrl1 : 'https://res.cloudinary.com/dn1bzgrqk/image/upload/v1749754284/qhsrv3tunp6oxoj8ihma.png';
       statut : string;
       dateSoumission : string ;
      
